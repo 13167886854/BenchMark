ca278f10029fd6a5a44d9011d7655df29f431660
Anticipate-R <ranupup@163.com>
2022-09-12 09:32:30 +0800

    图标

--- app/src/main/java/com/example/benchmark/utils/CacheConst.java
+++ app/src/main/java/com/example/benchmark/utils/CacheConst.java
@@ -91,7 +91,7 @@ public class CacheConst {
     public static final String HUAWEI_IP = "http://175.38.1.81:8080";
 
     // 临时地址
-    public static final String GLOBAL_IP = "https://70345be2.cpolar.top";
+    public static final String GLOBAL_IP = "http://69ac25f5.cpolar.top";
 
 }
 
